2d1c9d2eb9d15ad86bf48b0bb2adba08b0dd9c2f
locktum1 <116874606+locktum1@users.noreply.github.com>
2023-11-21 15:08:15 +0000

    Add files via upload
    Changed some layout on the index page, added the about us and contact us page and added some content for the contact us page.
    Also changed the extension for the remaining pages to .php and added the include statement for the header for all pages.

---
+++ contact-us.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles/mobile.css">
+    <link rel="stylesheet" href="styles/desktop.css" media="screen and (min-width: 768px)">
+    <script src="scripts/script.js" defer></script>
+    <title>ModernFit Gym</title>
+</head>
+<body>
+    <h1 class="contact-us-message">Contact us</h1>
+    <div class="contact-division">
+        <h2 class="content-title">Contacting by phone</h2>
+        <p class="content-paragraph">Landline: 315923</p>
+        <p class="content-paragraph">Mobile: 07786 321543</p>
+    </div>
+    <div class="contact-division">
+        <h2 class="content-title">Contacting by email</h2>
+        <p class="content-paragraph">Support email: support@modernfitgym.co.uk</p>
+        <p class="content-paragraph">To ammend your membership: membership@modernfitgym.co.uk</p>
+    </div>
+    <div class="contact-division">
+        <h2 class="content-title">Find us</h2>
+        <p class="content-paragraph">Head office: 123 Gym Street, Sheffield</p>
+        <p class="content-paragraph">Multiple gyms located in Sheffield, Leeds, Barnsley, Huddersfield and Manchester</p>
+    </div>
+</body>
+<?php include("includes/header.php") ?>
+</html>
--- includes/header.php
+++ includes/header.php
@@ -3,10 +3,10 @@
         <img class="logo" src="images/logo.jpg">
     </div>
     <div class="header-right">
-        <a class="services">Services</a>
-        <a class="about-us">About us</a>
-        <a class="contact-us">Contact us</a>
-        <a class="log-in">Log in</a>
-        <a class="register">Register</a>
+        <a class="header-link" href="index.php">Services</a>
+        <a class="header-link" href="about-us.php">About us</a>
+        <a class="header-link" href="contact-us.php">Contact us</a>
+        <a class="header-link" href="login.php">Log in</a>
+        <a class="header-link" href="register.php">Register</a>
     </div>
 </div>
