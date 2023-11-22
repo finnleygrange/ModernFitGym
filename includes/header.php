c6e465bc1acb07b591807485383c23ec4f37e3ac
Finnley Grange <dev.finnleygrange@icloud.com>
2023-11-22 15:21:06 +0000

    organisation and improved header

--- includes/header.php
+++ includes/header.php
@@ -1,14 +1,18 @@
-<header>
+<div class="header-main">
     <div class="header-left">
-        <img src="#" alt="Logo">
+        <a href="index.php">
+            <i class="fa-solid fa-dumbbell fa-bounce fa-lg" style="color: #6494ed;"></i>
+            <p>ModernFit Gym</p>
+        </a>
     </div>
     <div class="header-right">
         <ul>
             <li><a href="index.php">Home</a></li>
+            <li><a href="about-us.php">About Us</a></li>
             <li><a href="services.php">Services</a></li>
-            <li><a href="contact.php">Contact</a></li>
+            <li><a href="contact-us.php">Contact</a></li>
             <li><a href="login.php">Login</a></li>
             <li><a href="register.php">Register</a></li>
         </ul>
     </div>
-</header>
+</div>
