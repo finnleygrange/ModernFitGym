4586e9e5b618439b55960bfcadbfb2276880f389
musicjoe12 <josephgrierson8@gmail.com>
2023-11-22 20:47:12 +0000

    registration progress

--- register.php
+++ register.php
@@ -1,8 +1,34 @@
 <?php include("includes/head.php") ?>
 <?php include("includes/header.php") ?>
 
+<?php
+include ("php/config.php");
+if (isset($_POST['submit'])) {
+    $email = $_POST['email'];
+    $name = $_POST['name'];
+    $password = $_POST['password'];
+
+    $verify_query = mysqli_query($con, "SELECT Email FROM users WHERE Email='$email'");
+
+    if (mysqli_num_rows($verify_query) != 0) {
+        echo "<div class='message'>
+                <p>This email has already been used.</p>
+              </div> <br>";
+        echo "<a href='javascript:self.history.back()'><button class='btn'>Go Back</button></a>";
+    } else {
+        mysqli_query($con, "INSERT INTO users(Email, Name, Password) VALUES('$email','$name','$password')") or die("Error");
+
+        echo "<div class='message'>
+                <p>Registration Successful!.</p>
+              </div> <br>";
+        echo "<script>setTimeout(function(){ window.location.href = 'login.php'; }, 2000);</script>";
+    }
+} else {
+
+?>
+
 <div class="main-container">
-    <form action="">
+    <form method="post" action="">
         <div class="form-group">
             <h2 class="form-title">Create a new account</h2>
         </div>
@@ -24,12 +50,13 @@
                 required>
         </div>
         <div class="form-group">
-            <input type="submit" value="Register" id="register">
+            <input type="submit" value="Register" name="submit" id="register">
         </div>
     </form>
     <div class="form-group">
         <p class="login-here">Already have an account? <a href="login.php">Login Here</a></p>
     </div>
 </div>
+<?php } ?>
 
 <?php include("includes/footer.php") ?>
