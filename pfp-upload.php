72189ee2a834e2d188b1a388baf4b7cc4d5aa256
locktum1 <116874606+locktum1@users.noreply.github.com>
2023-12-08 11:35:10 +0000

    Finished merging mine and Finnley's files + pfp
    Finished the pfp uploading function - profile pictures can now be uploaded and be saved on the website.
    The files have also been merged so all changes are present.

--- pfp-upload.php
+++ pfp-upload.php
@@ -1,29 +1,48 @@
 <?php 
 
-$file = $_FILES["file"]["name"];
-$allowedFileSize = 500000;
+$file = $_FILES["file"]["name"]; //Name of file
+$fileSpacesRemoved = str_replace(" ","_", $file);
+$fileDir = "images/" . basename($_FILES["file"]["name"]); //File path
+$fileType = strtolower(pathinfo($file,PATHINFO_EXTENSION)); //File type
+$allowedFileSize = 500000; //Max file size
 
 $fileAllowed = 1;
+$errorMsg;
 
 $fileSizeCheck = getimagesize($_FILES["file"]["tmp_name"]);
-if ($fileSizeCheck !== false){
-    //Image is real
-} else {
+if ($fileSizeCheck == false){
     //Image is fake
     $fileAllowed = 0;
+    $errorMsg = "Upload failed";
 }
 
 if ($_FILES["file"]["size"] > $allowedFileSize){
     //File is too big
     $fileAllowed = 0;
+    $errorMsg = "Image size too large";
 }
 
-if (file_exists($file)){
+if (file_exists($fileDir)){
     echo "file exists";
-} else {
-    echo "doesnt exist";
+    $fileAllowed = 0;
+    $errorMsg = "File already exists";
 }
 
-//header("Location: ".$_SERVER['HTTP_REFERER']);//Redirects back to profile page.
+if ($fileType != "png" && $fileType != "jpg" && $fileType != "jpeg"){
+    //File is not an allowed filetype
+    $fileAllowed = 0;
+    $errorMsg = "File type not allowed";
+}
+
+if ($fileAllowed == 0){
+    session_start();
+    $_SESSION["errorMsg"] = $errorMsg;
+    $_SESSION["profilePicture"] = $fileDir;
+} else {
+    $fileDir = "images/" . basename($_FILES["file"]["name"]);
+    move_uploaded_file(($_FILES["file"]["tmp_name"]), $fileDir);
+}
+
+header("Location: ".$_SERVER['HTTP_REFERER']);//Redirects back to profile page.
 
 ?>
