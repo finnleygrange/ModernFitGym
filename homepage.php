bb2486ce964985dde62d55142573d9527992c96c
musicjoe12 <josephgrierson8@gmail.com>
2023-12-12 15:04:37 +0000

    css fix

--- homepage.php
+++ homepage.php
@@ -1,11 +1,7 @@
-<?php 
-include("includes/views/header.php"); 
-?>
-<div>
+<?php include("includes/views/header.php"); ?>
+<div class="homepage">
     <h1>Homepage</h1>
     <link rel="stylesheet" href="homepage.css">
-    <img src="woman.jpeg" alt="Woman working out">
+    <img src="images/woman.jpeg" alt="Woman working out" class="resizable-image">
 </div>
-
-
 <?php include("includes/views/footer.php") ?>
