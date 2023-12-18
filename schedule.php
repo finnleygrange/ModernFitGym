7173d356227469ea20f3812df245100babe89053
Finnley Grange <dev.finnleygrange@icloud.com>
2023-12-18 22:12:11 +0000

    css fixed for schedule

--- schedule.php
+++ schedule.php
@@ -1,7 +1,8 @@
 <?php include("includes/views/header.php"); ?>
 
-<div class="schedule-container">
-        <h1 id = "h1-schedule">Weekly Gym Schedule</h1>
+<div class="main-container">
+    <div class="schedule-container">
+        <h1 id="h1-schedule">Weekly Gym Schedule</h1>
         <table class="schedule-table">
             <thead>
                 <tr>
@@ -15,47 +16,48 @@
             </thead>
             <tbody>
                 <tr>
-                        <td class="day">Monday</td>
-                        <td class="workout">Chest Workout</td>
-                        <td class="workout">Cardio</td>
-                        <td class="workout">Yoga</td>
-                        <td class="workout"></td>
-                        <td class="workout"></td>
-                    </tr>
-                    <tr>
-                        <td class="day">Tuesday</td>
-                        <td class="workout">Back Workout</td>
-                        <td class="workout">HIIT</td>
-                        <td class="workout">Stretching</td>
-                        <td class="workout"></td>
-                        <td class="workout"></td>
-                    </tr>
-                    <tr>
-                        <td class="day">Wednesday</td>
-                        <td class="workout">Leg Workout</td>
-                        <td class="workout">Cardio</td>
-                        <td class="workout">Core Training</td>
-                        <td class="workout"></td>
-                        <td class="workout"></td>
-                    </tr>
-                    <tr>
-                        <td class="day">Thursday</td>
-                        <td class="workout">Shoulder Workout</td>
-                        <td class="workout">Cardio</td>
-                        <td class="workout">Pilates</td>
-                        <td class="workout"></td>
-                        <td class="workout"></td>
-                    </tr>
-                    <tr>
-                        <td class="day">Friday</td>
-                        <td class="workout">Arm Workout</td>
-                        <td class="workout">HIIT</td>
-                        <td class="workout">Yoga</td>
-                        <td class="workout"></td>
-                        <td class="workout"></td>
-                    </tr>
+                    <td class="day">Monday</td>
+                    <td class="workout">Chest Workout</td>
+                    <td class="workout">Cardio</td>
+                    <td class="workout">Yoga</td>
+                    <td class="workout"></td>
+                    <td class="workout"></td>
+                </tr>
+                <tr>
+                    <td class="day">Tuesday</td>
+                    <td class="workout">Back Workout</td>
+                    <td class="workout">HIIT</td>
+                    <td class="workout">Stretching</td>
+                    <td class="workout"></td>
+                    <td class="workout"></td>
+                </tr>
+                <tr>
+                    <td class="day">Wednesday</td>
+                    <td class="workout">Leg Workout</td>
+                    <td class="workout">Cardio</td>
+                    <td class="workout">Core Training</td>
+                    <td class="workout"></td>
+                    <td class="workout"></td>
+                </tr>
+                <tr>
+                    <td class="day">Thursday</td>
+                    <td class="workout">Shoulder Workout</td>
+                    <td class="workout">Cardio</td>
+                    <td class="workout">Pilates</td>
+                    <td class="workout"></td>
+                    <td class="workout"></td>
+                </tr>
+                <tr>
+                    <td class="day">Friday</td>
+                    <td class="workout">Arm Workout</td>
+                    <td class="workout">HIIT</td>
+                    <td class="workout">Yoga</td>
+                    <td class="workout"></td>
+                    <td class="workout"></td>
+                </tr>
             </tbody>
         </table>
     </div>
+</div>
 
 <?php include("includes/views/footer.php") ?>
