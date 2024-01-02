649e065bddc994927e5f92ec3c2aeaa077acbff3
Samuel12372 <samleoknowles@outlook.com>
2024-01-02 12:07:28 +0000

    Homepage Done

--- homepage.php
+++ homepage.php
@@ -1,7 +1,51 @@
 <?php include("includes/views/header.php"); ?>
 <div class="homepage">
-    <h1>Homepage</h1>
-    <link rel="stylesheet" href="homepage.css">
-    <img src="images/woman.jpeg" alt="Woman working out" class="resizable-image">
+    
+    <div class = "homepage-img">
+        <img src="images/gym-banner.jpg" alt="">
+        
+        <h1>Welcome To ModernFit Gym</h1>
+    </div>    
+       
+        <div class = "homepage-container-left">
+        <p>At ModernFit Gym in Sheffield, 
+            we're committed to transforming your
+            fitness journey into an empowering 
+            and rewarding experience. Our state-of-the-art 
+            facilities and personalized features are
+            designed to support you on your path to 
+            a healthier, stronger, and more vibrant life.<br>
+        <br>The new ModernFit Gym website is packed to the brim with new features
+        like --></p>
+        </div>
+        <div class = "homepage-container-center">
+        <p><u>Nutritional Table</u><br>
+        Explore our comprehensive nutritional table, 
+        crafted to guide your dietary choices and 
+        optimize your energy levels for peak performance. 
+        Whether you're aiming for muscle gain, weight loss,
+        or overall well-being, our nutritional insights 
+        will help you make informed decisions.<br>
+
+        <br><u>Meal & Workout Logging</u><br>
+        Take control of your fitness routine wit
+        our easy-to-use meal and workout logging tools. 
+        Log your daily meals, track your workouts, 
+        and gain valuable insights into your progress. 
+        Our user-friendly interface makes it simple to 
+        stay accountable and make adjustments to your routine as needed.
+
+        </div>
+        <div class = "homepage-container-right">
+        <p><U>Progress Tracking</U><br>
+        Celebrate your achievements with our progress tracking area.
+         Set personalized fitness goals, track your milestones, 
+         and witness the positive transformations in your strength, 
+         endurance, and overall well-being. We're here to celebrate 
+         every step of your journey.<br>
+
+        <Br>Ready to embark on your fitness adventure? Join ModernFit Gym today and experience the difference.<br>
+        <a href="register-page.php">JOIN HERE</a>
+        </div>
 </div>
 <?php include("includes/views/footer.php") ?>
